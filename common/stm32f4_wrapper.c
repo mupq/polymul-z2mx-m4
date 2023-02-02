cbaf8cf11aab65b09c40253195d2f495a4369e81
Matthias J. Kannwischer <matthias@kannwischer.eu>
2023-02-02 10:56:55 +0800

    update libopencm3

--- common/stm32f4_wrapper.c
+++ common/stm32f4_wrapper.c
@@ -2,29 +2,32 @@
 
 /* 24 MHz */
 const struct rcc_clock_scale benchmarkclock = {
-    .pllm = 8, //VCOin = HSE / PLLM = 1 MHz
-    .plln = 192, //VCOout = VCOin * PLLN = 192 MHz
-    .pllp = 8, //PLLCLK = VCOout / PLLP = 24 MHz (low to have 0WS)
-    .pllq = 4, //PLL48CLK = VCOout / PLLQ = 48 MHz (required for USB, RNG)
-    .hpre = RCC_CFGR_HPRE_DIV_NONE,
-    .ppre1 = RCC_CFGR_PPRE_DIV_2,
-    .ppre2 = RCC_CFGR_PPRE_DIV_NONE,
-    .flash_config = FLASH_ACR_DCEN | FLASH_ACR_ICEN | FLASH_ACR_LATENCY_0WS,
-    .apb1_frequency = 12000000,
-    .apb2_frequency = 24000000,
+  .pllm = 8, //VCOin = HSE / PLLM = 1 MHz
+  .plln = 192, //VCOout = VCOin * PLLN = 192 MHz
+  .pllp = 8, //PLLCLK = VCOout / PLLP = 24 MHz (low to have 0WS)
+  .pllq = 4, //PLL48CLK = VCOout / PLLQ = 48 MHz (required for USB, RNG)
+  .pllr = 0,
+  .hpre = RCC_CFGR_HPRE_DIV_NONE,
+  .ppre1 = RCC_CFGR_PPRE_DIV_2,
+  .ppre2 = RCC_CFGR_PPRE_DIV_NONE,
+  .pll_source = RCC_CFGR_PLLSRC_HSE_CLK,
+  .voltage_scale = PWR_SCALE1,
+  .flash_config = FLASH_ACR_DCEN | FLASH_ACR_ICEN | FLASH_ACR_LATENCY_0WS,
+  .ahb_frequency = 24000000,
+  .apb1_frequency = 12000000,
+  .apb2_frequency = 24000000,
 };
 
 void clock_setup(const enum clock_mode clock)
 {
-    switch(clock)
-    {
-        case CLOCK_BENCHMARK:
-            rcc_clock_setup_hse_3v3(&benchmarkclock);
-            break;
-        case CLOCK_FAST:
-        default:
-            rcc_clock_setup_hse_3v3(&rcc_hse_8mhz_3v3[RCC_CLOCK_3V3_168MHZ]);
-            break;
+    switch(clock) {
+    case CLOCK_BENCHMARK:
+      rcc_clock_setup_pll(&benchmarkclock);
+      break;
+    case CLOCK_FAST:
+    default:
+      rcc_clock_setup_pll(&rcc_hse_8mhz_3v3[RCC_CLOCK_3V3_168MHZ]);
+      break;
     }
 
     rcc_periph_clock_enable(RCC_GPIOA);
